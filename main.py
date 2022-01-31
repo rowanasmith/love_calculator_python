78cd63f0a368344f22f1d8b3d61308a22805c2ae
rowanasmith <45500732+rowanasmith@users.noreply.github.com>
2022-01-31 11:25:23 +0000

    First pass to get result

--- main.py
+++ main.py
@@ -6,4 +6,18 @@ name2 = input("What is their name? \n")
 
 #Write your code below this line 👇
 
+lower_name_string = lower(name1 + name2)
 
+t = lower_name_string.count("t")
+r = lower_name_string.count("r")
+u = lower_name_string.count("u")
+e = lower_name_string.count("e")
+l = lower_name_string.count("l")
+o = lower_name_string.count("o")
+v = lower_name_string.count("v")
+
+digit_one = string(t + r + u + e)
+digit_two = string(l + o + v + e)
+result = int(digit_one + digit_two)
+
+print(result)
